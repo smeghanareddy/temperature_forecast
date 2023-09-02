3472d70dcce3519860623f0a4045d56f3b4987cb
unknown <solipuram.meghana@gmail.com>
2023-09-02 15:05:29 +0530

    Cache the forecast details for 30 minutes for all subsequent requests by zip codes.

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -7,10 +7,16 @@ class ForecastController < ApplicationController
     if params[:address]
       begin
         address = params[:address]
-        geocode = GeocodeService.call(address)
 
-        @weather = WeatherService.call(geocode.latitude, geocode.longitude)      
-        
+        # Converting full address details to Latitude & Longitude
+        geocode = GeocodeService.call(address)
+        weather_cache_key = "#{geocode.country_code}/#{geocode.postal_code}"
+        @weather_cache_exist = Rails.cache.exist?(weather_cache_key)
+
+        #Cache the forecast details for 30 minutes and forecasting weather details
+        @weather = Rails.cache.fetch(weather_cache_key, expires_in: 30.minutes) do
+          WeatherService.call(geocode.latitude, geocode.longitude)
+        end
       rescue => e
         flash.alert = e.message
       end
